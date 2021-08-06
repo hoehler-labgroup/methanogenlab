56d67a911c94161563514e4584712bcbb6208742
mankeldy <dylanmankel@gmail.com>
2021-08-06 11:21:26 -0700

    Fixed aqueous.step documentation

--- R/aqueous.R
+++ R/aqueous.R
@@ -1,10 +1,9 @@
 #' Uses Henry's Law to calculate dissolved gas concentrations
 #'
 #' `aqueous.step()` calculates the aqueous concentration at each step using the current pressure.
-#'
-#'  @param P.step the current pressure in the system.
-#'  @param K Henry's equilibrium constant of the gas.
-#'  @return Dissolved gas concentration, in molar.
+#' @param P.step the current pressure in the system.
+#' @param K Henry's equilibrium constant of the gas.
+#' @return Dissolved gas concentration, in molar.
 aqueous.step <- function(P.step, K){
   conc.step <- P.step * K
   return(conc.step)
